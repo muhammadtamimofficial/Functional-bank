1a7cab5e9a5f15a8b27adcc03c22d34a6e968031
muhammad-tamim <mdt40234@gmail.com>
2021-08-17 17:22:50 +0600

    update total balance

--- js/banking.js
+++ js/banking.js
@@ -6,7 +6,12 @@ function getInputValue(inputId) {
     inputFild.value = '';
     return amountValue;
 }
-
+function updateTotalFild(TotalFoldId, Amount) {
+    const totalElement = document.getElementById(TotalFoldId);
+    const TotalText = totalElement.innerText;
+    const previouseTotal = parseFloat(TotalText);
+    totalElement.innerText = previouseTotal + Amount;
+}
 document.getElementById('Deposit-btn').addEventListener('click', function () {
     /*     const DepositInput = document.getElementById('input-Deposit');
         const DepositAmountText = DepositInput.value;
@@ -14,10 +19,11 @@ document.getElementById('Deposit-btn').addEventListener('click', function () {
     const DepositAmount = getInputValue('input-Deposit');
 
     // Get currant Deposit
-    const depositTotal = document.getElementById('total-deposit');
+    /* const depositTotal = document.getElementById('total-deposit');
     const depositTotalText = depositTotal.innerText;
     const previouseDepositTotal = parseFloat(depositTotalText);
-    depositTotal.innerText = previouseDepositTotal + DepositAmount;
+    depositTotal.innerText = previouseDepositTotal + DepositAmount; */
+    updateTotalFild('total-deposit', DepositAmount);
 
     // updated balance
     const balanceTotal = document.getElementById('total-amount');
@@ -33,20 +39,20 @@ document.getElementById('widthrow-btn').addEventListener('click', function () {
     const widthrowAmount = parseFloat(widthrowAmountText);
      */
     const widthrowAmount = getInputValue('input-withdrow');
-    console.log(widthrowAmount);
 
     // Get currant withdrow
-    const withdrowTotal = document.getElementById('withdrow-total');
+    /* const withdrowTotal = document.getElementById('withdrow-total');
     const withdrowTotalText = withdrowTotal.innerText;
     const previousWitdrowTotal = parseFloat(withdrowTotalText)
-    withdrowTotal.innerText = previousWitdrowTotal + widthrowAmount;
-    console.log(withdrowTotal);
+    
+
+    withdrowTotal.innerText = previousWitdrowTotal + widthrowAmount; */
+
+    updateTotalFild('withdrow-total', widthrowAmount);
 
     // Get currant balanceTotal
     const balanceTotal = document.getElementById('total-amount');
     const balanceTotalText = balanceTotal.innerText;
     const PreviousBalanceTotal = parseFloat(balanceTotalText);
     balanceTotal.innerText = PreviousBalanceTotal - widthrowAmount;
-
-    console.log(PreviousBalanceTotal);
 })
